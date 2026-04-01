3eada10d7f89cdb83d15b57fd6e3b112f8640289
Danh Thanh <thanhddhe181468@fpt.edu.vn>
2026-04-01 13:19:22 +0700

    feat(extra-fees): enhance filtering and validation for extra fees management

--- api/src/controllers/extraFeeController.js
+++ api/src/controllers/extraFeeController.js
@@ -8,7 +8,24 @@ import { HttpStatus } from '../utils/httpStatus.js';
 // @route   GET /api/v1/extra-fees
 // @access  Private/Admin
 const getExtraFees = catchAsync(async (req, res) => {
-	const extraFees = await ExtraFee.find().populate('hotelId', 'name');
+	const { hotelId, minPrice, maxPrice } = req.query;
+	const filter = { isDeleted: false };
+
+	if (hotelId && hotelId !== 'all') {
+		filter.hotelId = hotelId;
+	}
+
+	if (minPrice !== undefined || maxPrice !== undefined) {
+		filter.extraPrice = {};
+		if (minPrice !== undefined && minPrice !== '') {
+			filter.extraPrice.$gte = Number(minPrice);
+		}
+		if (maxPrice !== undefined && maxPrice !== '') {
+			filter.extraPrice.$lte = Number(maxPrice);
+		}
+	}
+
+	const extraFees = await ExtraFee.find(filter).populate('hotelId', 'name');
 	res.status(HttpStatus.OK).json({
 		success: true,
 		data: extraFees,
@@ -19,7 +36,10 @@ const getExtraFees = catchAsync(async (req, res) => {
 // @route   GET /api/v1/extra-fees/:id
 // @access  Private/Admin
 const getExtraFeeById = catchAsync(async (req, res, next) => {
-	const extraFee = await ExtraFee.findById(req.params.id);
+	const extraFee = await ExtraFee.findOne({
+		_id: req.params.id,
+		isDeleted: false,
+	});
 
 	if (extraFee) {
 		res.status(HttpStatus.OK).json({
@@ -34,13 +54,18 @@ const getExtraFeeById = catchAsync(async (req, res, next) => {
 // @desc    Create an extra fee
 // @route   POST /api/v1/extra-fees
 // @access  Private/Admin
-const createExtraFee = catchAsync(async (req, res) => {
+const createExtraFee = catchAsync(async (req, res, next) => {
 	const { hotelId, extraName, extraPrice } = req.body;
+	const parsedPrice = Number(extraPrice);
+
+	if (Number.isNaN(parsedPrice) || parsedPrice < 0) {
+		return next(new AppError(HttpStatus.BAD_REQUEST, 'Extra price is invalid'));
+	}
 
 	const extraFee = new ExtraFee({
 		hotelId,
 		extraName,
-		extraPrice,
+		extraPrice: parsedPrice,
 	});
 
 	const createdExtraFee = await extraFee.save();
@@ -57,7 +82,7 @@ const updateExtraFee = catchAsync(async (req, res, next) => {
 	const { id } = req.params;
 	const { hotelId, extraName, extraPrice } = req.body;
 
-	const extraFee = await ExtraFee.findById(id);
+	const extraFee = await ExtraFee.findOne({ _id: id, isDeleted: false });
 
 	if (!extraFee) {
 		return next(new AppError(HttpStatus.NOT_FOUND, 'Extra fee not found'));
@@ -80,7 +105,15 @@ const updateExtraFee = catchAsync(async (req, res, next) => {
 
 	extraFee.hotelId = hotelId || extraFee.hotelId;
 	extraFee.extraName = extraName || extraFee.extraName;
-	extraFee.extraPrice = extraPrice !== undefined ? extraPrice : extraFee.extraPrice;
+	if (extraPrice !== undefined) {
+		const parsedPrice = Number(extraPrice);
+		if (Number.isNaN(parsedPrice) || parsedPrice < 0) {
+			return next(
+				new AppError(HttpStatus.BAD_REQUEST, 'Extra price is invalid'),
+			);
+		}
+		extraFee.extraPrice = parsedPrice;
+	}
 
 	const updatedExtraFee = await extraFee.save();
 	res.status(HttpStatus.OK).json({
@@ -94,7 +127,7 @@ const updateExtraFee = catchAsync(async (req, res, next) => {
 // @access  Private/Admin
 const deleteExtraFee = catchAsync(async (req, res, next) => {
 	const { id } = req.params;
-	const extraFee = await ExtraFee.findById(id);
+	const extraFee = await ExtraFee.findOne({ _id: id, isDeleted: false });
 
 	if (!extraFee) {
 		return next(new AppError(HttpStatus.NOT_FOUND, 'Extra fee not found'));
@@ -115,10 +148,12 @@ const deleteExtraFee = catchAsync(async (req, res, next) => {
 		);
 	}
 
-	await ExtraFee.deleteOne({ _id: id });
+	extraFee.isDeleted = true;
+	extraFee.deletedAt = new Date();
+	await extraFee.save();
 	res.status(HttpStatus.OK).json({
 		success: true,
-		message: 'Extra fee removed',
+		message: 'Extra fee archived',
 	});
 });
 

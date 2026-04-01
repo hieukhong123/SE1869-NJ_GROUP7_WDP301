afc84190cdc8358116636b9493d7172eee2cc515
Danh Thanh <thanhddhe181468@fpt.edu.vn>
2026-04-01 16:28:06 +0700

    feat(error): enhance global error handler for JWT and token expiration errors

--- api/src/middlewares/errorMiddleware.js
+++ api/src/middlewares/errorMiddleware.js
@@ -1,6 +1,18 @@
 import { HttpStatus } from '../utils/httpStatus.js';
 
 export const globalErrorHandler = (err, req, res, next) => {
+	if (err.name === 'JsonWebTokenError') {
+		err.statusCode = HttpStatus.UNAUTHORIZED;
+		err.status = 'fail';
+		err.message = 'Invalid token. Please log in again.';
+	}
+
+	if (err.name === 'TokenExpiredError') {
+		err.statusCode = HttpStatus.UNAUTHORIZED;
+		err.status = 'fail';
+		err.message = 'Session expired. Please log in again.';
+	}
+
 	err.statusCode = err.statusCode || HttpStatus.INTERNAL_SERVER_ERROR;
 	err.status = err.status || 'error';
 
